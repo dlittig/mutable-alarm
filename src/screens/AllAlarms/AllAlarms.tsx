3b28a8b8ba08f9a5fd652b86c54f30a3836f82c8
dlittig <david0605.littig@hotmail.de>
2020-01-26 12:59:21 +0100

    Removed keyExtractor as is doesnt solve that the warning is showing up.

--- src/screens/AllAlarms/AllAlarms.tsx
+++ src/screens/AllAlarms/AllAlarms.tsx
@@ -9,42 +9,50 @@ const data = [
   {
     id: 1,
     time: "09:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 2,
     time: "11:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 3,
     time: "13:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 4,
     time: "15:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 5,
     time: "17:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 6,
     time: "19:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 7,
     time: "20:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   },
   {
     id: 8,
     time: "22:00",
-    isEnabled: true
+    isEnabled: true,
+    isSnoozed: false
   }
 ];
 
@@ -68,10 +76,10 @@ const AllAlarms = () => (
           id={item.id}
           time={item.time}
           isEnabled={item.isEnabled}
+          isSnoozed={item.isSnoozed}
           isMuted={false}
         />
       )}
-      keyExtractor={item => item.id}
     />
     <FAB
       style={styles.fab}
