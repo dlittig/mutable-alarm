cb475059453cd89da5196e0b317597272c1c8fd9
dlittig <david0605.littig@hotmail.de>
2020-11-19 07:11:53 +0100

    Added more translations to the lang files.

--- src/translations/de-DE/index.ts
+++ src/translations/de-DE/index.ts
@@ -8,7 +8,45 @@ export const de_DE = {
       reset: "Zurücksetzen",
       stop: "Stop",
       lap: "Runde",
-      save: "Speichern"
+      save: "Speichern",
+      edit: "Bearbeiten",
+      delete: "Löschen",
+      mute: "Stumm",
+      unmute: "Aktivieren",
+    },
+    screens: {
+      all_alarms: "Alle Alarme",
+      muted_alarms: "Stumme Alarme",
+      add_alarm: "Alarm hinzufügen",
+      alarms: "Alarme",
+      timer: "Timer",
+      stopwatch: "Stoppuhr",
+      settings: "Einstellungen",
+      donate: "Spenden",
+      about: "Über",
+    },
+    descriptions: {
+      options: {
+        donate: "Kauf' mir einen Kaffee",
+        settings: "Anwendungsverhalten einstellen",
+        about: "Lizenzen und weitere Informationen",
+      },
+      settings: {
+        display_theme: "Passe an, wie die App aussehen soll",
+        automatic: "Die App ändert das Farbschema basierend auf den Einstellungen deines Smartphones"
+      },
+    },
+    dialogs: {
+      timer: {
+        set: "Timer setzen",
+      },
+    },
+    settings: {
+      display_theme: "Farbschema",
+      light: "Hell",
+      dark: "Dunkel",
+      black: "Schwarz",
+      automatic: "Automatisch"
     },
   },
 };
--- src/translations/en-US/index.ts
+++ src/translations/en-US/index.ts
@@ -8,7 +8,46 @@ export const en_US = {
       reset: "Reset",
       stop: "Stop",
       lap: "Lap",
-      save: "Save"
+      save: "Save",
+      edit: "Edit",
+      delete: "Delete",
+      mute: "Mute",
+      unmute: "Unmute",
+    },
+    screens: {
+      all_alarms: "All Alarms",
+      muted_alarms: "Muted Alarms",
+      add_alarm: "Add Alarm",
+      alarms: "Alarms",
+      timer: "Timer",
+      stopwatch: "Stopwatch",
+      settings: "Settings",
+      donate: "Donate",
+      about: "About",
+    },
+    descriptions: {
+      options: {
+        donate: "Buy me a coffee",
+        settings: "Configure app behaviour",
+        about: "License and more info",
+      },
+      settings: {
+        display_theme: "Adjust how the appearance of the app should look like",
+        automatic:
+          "The app will change its appearance based on your phone's settings",
+      },
+    },
+    dialogs: {
+      timer: {
+        set: "Set timer",
+      },
+    },
+    settings: {
+      display_theme: "Display theme",
+      light: "Light",
+      dark: "Dark",
+      black: "Black",
+      automatic: "Automatic",
     },
   },
 };
